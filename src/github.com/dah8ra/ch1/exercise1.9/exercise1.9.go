295ff56b9c868f9ff4445ed178ec98828e27cdfa
dah.hara <dah.hara@gmail.com>
2016-01-30 16:13:26 +0900

    Added exercise1.9 in ch1.

---
+++ src/github.com/dah8ra/ch1/exercise1.9/exercise1.9.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	httpStr := "http://"
+	for _, url := range os.Args[1:] {
+		if strings.HasPrefix(url, httpStr) == false {
+			fmt.Println("Added http prefix!!!")
+			url = httpStr + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		status := resp.Status
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("ResponseCode: %s\n============================\n%s", status, b)
+	}
+}
