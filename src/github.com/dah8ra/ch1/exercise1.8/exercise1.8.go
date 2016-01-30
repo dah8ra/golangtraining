e89b2a054b5a9144eedc1bdec06e0fc170c57397
dah.hara <dah.hara@gmail.com>
2016-01-30 16:04:19 +0900

    Added excersize1.8 in ch1.

---
+++ src/github.com/dah8ra/ch1/exercise1.8/exercise1.8.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"bufio"
+	"strings"
+)
+
+
+
+func main() {
+	httpStr := "http://"
+	for _, url := range os.Args[1:] {
+		if strings.HasPrefix(url, httpStr) == false {
+			fmt.Println("Added http prefix!!!")
+			url = httpStr + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		w := bufio.NewWriter(os.Stdout);
+		_, err = io.Copy(w, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
