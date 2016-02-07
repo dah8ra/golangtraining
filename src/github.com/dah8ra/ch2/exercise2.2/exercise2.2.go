40a4d0256e7244ecb8ed61ab94d3e14be68aaa84
dah.hara <dah.hara@gmail.com>
2016-02-07 15:46:43 +0900

    Added exercise2.2 in ch2.

---
+++ src/github.com/dah8ra/ch2/exercise2.2/exercise2.2.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"bufio"
+	"github.com/dah8ra/ch2/tempconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err == nil {
+			f := tempconv.Fahrenheit(t)
+			c := tempconv.Celsius(t)
+			fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+		} else if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+	}	
+	in := bufio.NewScanner(os.Stdin)
+	for in.Scan() {
+		inStr := in.Text()
+		t, _ := strconv.ParseFloat(inStr, 64)
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
+}		
+	
