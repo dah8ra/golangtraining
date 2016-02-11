8bce48c447288dc7901c87b2db42254111b8b7ef
dah.hara <dah.hara@gmail.com>
2016-02-11 20:38:36 +0900

    Added popcount code in ch2.

---
+++ src/github.com/dah8ra/ch2/popcount/popcount.go
@@ -0,0 +1,32 @@
+package popcount
+
+import (
+	"fmt"
+)
+
+var pc [256]byte
+
+func init(){
+	fmt.Println("Call init()")
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+		//fmt.Print(byte(i&1))
+		//fmt.Print(i/2)
+		fmt.Print(pc[i])
+	}
+}
+
+func Test(x uint64) int {
+	return int(pc[byte(x>>(0*8))])
+}
+
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+	pc[byte(x>>(1*8))] +
+	pc[byte(x>>(2*8))] +
+	pc[byte(x>>(3*8))] +
+	pc[byte(x>>(4*8))] +
+	pc[byte(x>>(5*8))] +
+	pc[byte(x>>(6*8))] +
+	pc[byte(x>>(7*8))]) 
+}
