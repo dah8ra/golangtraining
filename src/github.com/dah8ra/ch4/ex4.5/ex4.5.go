ed1d6778b152e2849ad4a45f6d7e6dbc639fa258
dah.hara <dah.hara@gmail.com>
2016-02-20 21:11:02 +0900

    Added a method about deleting string only.

--- src/github.com/dah8ra/ch4/ex4.5/ex4.5.go
+++ src/github.com/dah8ra/ch4/ex4.5/ex4.5.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
 	var src = []string{"a", "a", "b", "c", "c", "c", "d", "e"}
+	fmt.Println(delete(src))
+}
+
+func deleteAndStuff(src []string) []string {
 	var temp string
 	result := []string{}
 	for _, v := range src {
@@ -12,5 +16,16 @@ func main() {
 		}
 		temp = v
 	}
-	fmt.Println(result)
+	return result
+}
+
+func delete(src []string) []string {
+	var temp string
+	for i, v := range src {
+		if temp != v {
+			src[i] = ""
+		}
+		temp = v
+	}
+	return src
 }
