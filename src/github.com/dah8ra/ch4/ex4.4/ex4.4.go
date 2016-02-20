34a4e0932b117afff8bb301d84c378fe9a7c84d4
dah.hara <dah.hara@gmail.com>
2016-02-20 19:57:53 +0900

    Added ex4.4.

---
+++ src/github.com/dah8ra/ch4/ex4.4/ex4.4.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	rotate(a[:], 3)
+	fmt.Println(a)
+}
+
+func rotate(p []int, rotate int) {
+	reverse(p[:rotate])
+	reverse(p[rotate:])
+	reverse(p[:])
+}
+
+func reverse(p []int) {
+	for i, j := 0, len(p)-1; i < j; i, j = i+1, j-1 {
+		p[i], p[j] = p[j], p[i]
+		fmt.Println(p[j])
+	}
+}
