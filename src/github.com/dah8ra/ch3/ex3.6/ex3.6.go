5473a12b36574c8cf1bad264bfeb2bcdf73f8a61
dah.hara <dah.hara@gmail.com>
2016-03-05 17:35:28 +0900

    Modified supersampling code.

--- src/github.com/dah8ra/ch3/ex3.6/ex3.6.go
+++ src/github.com/dah8ra/ch3/ex3.6/ex3.6.go
@@ -9,6 +9,10 @@ import (
 )
 
 func main() {
+	const (
+		blocksize = 4
+	)
+
 	file, err := os.Open("lena.jpg")
 	defer file.Close()
 	if err != nil {
@@ -20,17 +24,12 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	// resize for performance
-	//	img = resize.Resize(36, 0, img, resize.Lanczos3)
 
 	width, height := getImageDimension("lena.jpg")
-	fmt.Println("Width:", width, "Height:", height)
+	//fmt.Println("Width:", width, "Height:", height)
 
-	bsize := 4
-	w := width / bsize
-	h := height / bsize
-
-	outimg := getOutimg(w, h, bsize, img)
+	outimg := supersampling(width, height, blocksize, img)
+	//	outimg := getOutimg(width, height, blocksize, img)
 
 	var outFile *os.File
 	if outFile, err = os.Create("out.jpg"); err != nil {
@@ -41,12 +40,26 @@ func main() {
 	option := &jpeg.Options{Quality: 100}
 
 	if err = jpeg.Encode(outFile, outimg, option); err != nil {
-		//if err = jpeg.Encode(outFile, img, nil); err != nil {
-		println()
 		return
 	}
 
 	defer outFile.Close()
+}
+
+func supersampling(width int, height int, bsize int, img image.Image) image.Image {
+	rect := image.Rect(0, 0, width*bsize, height*bsize)
+	outimg := image.NewRGBA(rect)
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			c := img.At(x, y)
+			for i := 0; i < bsize; i++ {
+				for j := 0; j < bsize; j++ {
+					outimg.Set(x*bsize+i, y*bsize+j, c)
+				}
+			}
+		}
+	}
+	return outimg
 }
 
 func getOutimg(width int, height int, bsize int, img image.Image) image.Image {
@@ -61,49 +74,41 @@ func getOutimg(width int, height int, bsize int, img image.Image) image.Image {
 			if x > 1 && y > 1 && x < width && y < height {
 				top = img.At(x, y-1)
 				bottom = img.At(x, y+1)
-				//				center := img.At(x, y)
 				left = img.At(x-1, y)
 				right = img.At(x+1, y)
 			} else if x == 0 && y == 0 { // hidariue
 				//top := img.At(x,y-1)
 				bottom = img.At(x, y+1)
-				//				center := img.At(x, y)
 				//left := img.At(x-1,y)
 				right = img.At(x+1, y)
 			} else if x > 1 && x < width && y == 0 { // ue
 				//top := img.At(x, y-1)
 				bottom = img.At(x, y+1)
-				//				center := img.At(x, y)
 				left = img.At(x-1, y)
 				right = img.At(x+1, y)
 			} else if x == width && y == 0 { // migiue
 				//top := img.At(x, y-1)
 				bottom = img.At(x, y+1)
-				//				center := img.At(x, y)
 				left = img.At(x-1, y)
 				//right := img.At(x+1, y)
 			} else if x == 0 && y > 1 && y < height { //hidari
 				top = img.At(x, y-1)
 				bottom = img.At(x, y+1)
-				//				center := img.At(x, y)
 				//left := img.At(x-1, y)
 				right = img.At(x+1, y)
 			} else if x == 0 && y == height { // hidarishita
 				top = img.At(x, y-1)
 				//bottom := img.At(x, y+1)
-				//				center := img.At(x, y)
 				//left := img.At(x-1, y)
 				right = img.At(x+1, y)
 			} else if x > 1 && y == height && x < width { // shita
 				top = img.At(x, y-1)
 				//bottom := img.At(x, y+1)
-				//				center = img.At(x, y)
 				left = img.At(x-1, y)
 				right = img.At(x+1, y)
 			} else if x == width && y == height { // migishita
 				top = img.At(x, y-1)
 				//bottom := img.At(x, y+1)
-				//				center = img.At(x, y)
 				left = img.At(x-1, y)
 				//right := img.At(x+1, y)
 			}
@@ -128,19 +133,6 @@ func getNilToGrayRGBA(c color.Color) color.Color {
 	}
 	return c
 }
-
-//	max := 0
-// find most appered color
-//	for _, val := range hist {
-//		if val > max {
-//			max = val
-//		}
-//	}
-//	r, g, b := int2rgb(max)
-//	colorCode := "#" + dec2hex(r, 2) + dec2hex(g, 2) + dec2hex(b, 2)
-//
-//	 most appered color in image
-//	println(colorCode)
 
 func getSamplingColor(top color.Color, bottom color.Color, left color.Color, right color.Color) color.Color {
 	tr, tg, tb, ta := top.RGBA()
@@ -169,40 +161,4 @@ func getImageDimension(imagePath string) (int, int) {
 		fmt.Fprintf(os.Stderr, "%s: %v\n", imagePath, err)
 	}
 	return image.Width, image.Height
-}
-
-func getHistgram(img image.Image) []int {
-	hist := make([]int, 1000000)
-
-	// get bounds
-	rect := img.Bounds()
-	// color reduction
-	for i := 0; i < rect.Max.Y; i++ {
-		for j := 0; j < rect.Max.X; j++ {
-			r, g, b, _ := img.At(j, i).RGBA()
-			i := rgb2int(int(r), int(g), int(b))
-			hist[i]++
-		}
-	}
-	return hist
-}
-
-func dec2hex(n, beam int) string {
-	hex := ""
-	str := "0123456789abcdef"
-	for i := 0; i < beam; i++ {
-		m := n & 0xf
-		hex = string(str[m]) + hex
-		n -= m
-		n >>= 4
-	}
-	return hex
-}
-
-func rgb2int(r, g, b int) int {
-	return (((r >> 5) << 6) | ((g >> 5) << 3) | ((b >> 5) << 0))
-}
-
-func int2rgb(i int) (r, g, b int) {
-	return ((i >> 6 & 0x7) << 5) + 16, ((i >> 3 & 0x7) << 5) + 16, ((i >> 0 & 0x7) << 5) + 16
 }
