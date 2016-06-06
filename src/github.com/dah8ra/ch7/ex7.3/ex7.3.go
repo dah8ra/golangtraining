44df7133a94fe3445bb7263b935a37f2104fb976
dah.hara <dah.hara@gmail.com>
2016-06-06 15:25:43 +0900

    Added the code which are displayed by tree sort.

--- src/github.com/dah8ra/ch7/ex7.3/ex7.3.go
+++ src/github.com/dah8ra/ch7/ex7.3/ex7.3.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/dah8ra/ch7/treesort"
-//	"math/rand"
+	"math/rand"
 )
 
 func main() {
-	data := make([]int, 50)
+	data := make([]int, 8)
 	for i := range data {
-		//		data[i] = rand.Int() % 50
-		data[i] = i
+		data[i] = rand.Int() % 8
 	}
+	//	data[0] = 4
+	//	data[1] = 3
+	//	data[2] = 2
+	//	data[3] = 8
+	//	data[4] = 6
+	//	data[5] = 9
+	fmt.Printf("INPUT: %v\n", data)
 	root := treesort.Sort(data)
 	root.String()
 }
---
+++ src/github.com/dah8ra/ch7/treesort/sort.go
@@ -0,0 +1,69 @@
+package treesort
+
+import "fmt"
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+// Sort sorts values in place.
+func Sort(values []int) *tree {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+	return root
+}
+
+// appendValues appends the elements of t to values in order
+// and returns the resulting slice.
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		// Equivalent to return &tree{value: value}.
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
+
+func (t *tree) String() {
+	show(t, true, false, -1)
+}
+
+func show(t *tree, hasNext bool, init bool, indent int) {
+	if t == nil {
+		return
+	}
+	tab := ""
+	for i := 0; i < indent; i++ {
+		tab = tab + " "
+	}
+	if !init {
+		fmt.Printf("%d\n", t.value)
+	} else {
+		fmt.Printf("%s├%d\n", tab, t.value)
+	}
+	if t.left != nil {
+		show(t.left, true, true, indent+1)
+	}
+	if t.right != nil {
+		show(t.right, false, true, indent+1)
+	}
+}
