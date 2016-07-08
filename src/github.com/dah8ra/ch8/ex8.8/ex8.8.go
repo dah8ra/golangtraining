9aa26bf079a1e40c582b3104f83e220a7596791e
dah.hara <dah.hara@gmail.com>
2016-07-08 16:46:08 +0900

    Modified ex8.8.

--- src/github.com/dah8ra/ch8/ex8.8/ex8.8.go
+++ src/github.com/dah8ra/ch8/ex8.8/ex8.8.go
@@ -17,25 +17,26 @@ func echo(c net.Conn, shout string, delay time.Duration) {
 	fmt.Fprintln(c, "\t", strings.ToLower(shout))
 }
 
-func handleConn(c net.Conn) {
-	fmt.Println("Called handleConn")
-	tick := time.Tick(10 * time.Second)
+func handleConn(c net.Conn, token chan struct{}) {
 	input := bufio.NewScanner(c)
-	token := make(chan struct{})
 	for input.Scan() {
 		token <- struct{}{}
 		go echo(c, input.Text(), 1*time.Second)
 	}
+}
 
-	select {
-	case <-tick:
-		c.Close()
-		fmt.Println("Close connection...")
-	case <-token:
-		tick = time.Tick(10 * time.Second)
-		fmt.Println("Keep connection...")
+func wait(c net.Conn, token chan struct{}) {
+	for {
+		tick := time.Tick(10 * time.Second)
+		select {
+		case <-tick:
+			c.Close()
+			fmt.Println("Close connection...")
+		case <-token:
+			tick = time.Tick(10 * time.Second)
+			fmt.Println("Keep connection...")
+		}
 	}
-
 }
 
 func main() {
@@ -44,15 +45,13 @@ func main() {
 		log.Fatal(err)
 	}
 	for {
-		fmt.Println("@@@")
 		conn, err := l.Accept()
-		fmt.Println("@@@")
 		if err != nil {
 			log.Print(err) // e.g., connection aborted
 			continue
 		}
-		fmt.Println("@@@")
-		fmt.Println("Call handleConn")
-		go handleConn(conn)
+		token := make(chan struct{})
+		go handleConn(conn, token)
+		go wait(conn, token)
 	}
 }
