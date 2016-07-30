50c8c36482e88275fb62c707d353918d535a890d
dah.hara <dah.hara@gmail.com>
2016-07-30 16:22:02 +0900

    Added ex10.1.

---
+++ src/github.com/dah8ra/ch10/ex101/ex101.go
@@ -0,0 +1,42 @@
+// The jpeg command reads a PNG image from the standard input
+// and writes it as a JPEG image to the standard output.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"io"
+	"os"
+)
+
+var s = flag.String("type", "jpeg", "Select output image type. PNF, JPEG suported.")
+
+// $go run ex101.go -type jpeg < test.png > out.jpeg
+func main() {
+	flag.Parse()
+
+	if err := convert(os.Stdin, os.Stdout, *s); err != nil {
+		fmt.Sprintln("Type: %s\n", *s)
+		fmt.Fprintf(os.Stderr, "Specified output type: %s, err: %v\n", *s, err)
+		os.Exit(1)
+	}
+}
+
+func convert(in io.Reader, out io.Writer, s string) error {
+	img, kind, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "Input format =", kind)
+	if s == "jpeg" {
+		return jpeg.Encode(out, img, &jpeg.Options{Quality: 95})
+	} else if s == "png" {
+		return png.Encode(out, img)
+	} else if s == "bmp" {
+
+	}
+	return nil
+}
